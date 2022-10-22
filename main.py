ffb2786a719362e530cb420d99878e9223c17a20
VjayRam <vjayram.enag@gmail.com>
2022-10-22 17:58:40 +0530

    -

--- main.py
+++ main.py
@@ -12,7 +12,10 @@ def initialize_db():
 def init_table(db_name):
     conn = sql.connect(host='localhost',user='root',password='root',db = db_name)
     a = conn.cursor()
-    tab_n = input('Enter Table Name: ')
-    q = 'create table '+ tab_n +'(RegNo varchar(5) PRIMARY KEY, name varchar(25), sec varchar(2), gender varchar(2), dob date, );'
-    a.execute()
+    tab_n = input('Enter Class (1-12): ')
+    if tab_n.isnumeric()==True:
+        q = 'create table class'+ tab_n +'(RegNo varchar(5) PRIMARY KEY, name varchar(25), sec varchar(2), gender varchar(2), dob date, phno int(12));'
+        a.execute(q)
+    else:
+        print('ERROR: Enter only numbers as input')
     return
