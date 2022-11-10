ca6d97a278dd7cfba6c50bb977a9d1c97527d9cc
VjayRam <vjayram.enag@gmail.com>
2022-11-10 12:36:21 +0530

    -

--- attendance_db.sql
+++ attendance_db.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 25, 2015 at 12:02 PM
+-- Generation Time: Jan 24, 2015 at 08:17 AM
 -- Server version: 5.6.16
 -- PHP Version: 5.5.11
 
@@ -17,213 +17,306 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8 */;
 
 --
--- Database: `attendance_db`
+-- Database: `j2`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `attendance_table`
+-- Table structure for table `admin`
 --
 
-CREATE TABLE IF NOT EXISTS `attendance_table` (
-  `program` varchar(64) NOT NULL,
-  `semester` varchar(64) NOT NULL,
-  `subject` varchar(64) NOT NULL,
-  `teacher` varchar(64) NOT NULL,
-  `std_roll_no` int(11) NOT NULL,
-  `student_name` varchar(100) NOT NULL,
-  `attendance` varchar(8) NOT NULL,
-  `attd_date` date NOT NULL,
-  `TeacherId` int(11) DEFAULT NULL
+CREATE TABLE IF NOT EXISTS `admin` (
+  `admin_id` int(11) NOT NULL AUTO_INCREMENT,
+  `admin_name` varchar(40) NOT NULL,
+  `username` varchar(40) NOT NULL,
+  `password` varchar(130) NOT NULL,
+  PRIMARY KEY (`admin_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+
+--
+-- Dumping data for table `admin`
+--
+
+INSERT INTO `admin` (`admin_id`, `admin_name`, `username`, `password`) VALUES
+(1, 'Admin', 'temp', '1fcff248de3a0dd21de0ee4a37b7bd7e418323e7c1b8c03f5f2967a7a697f363d4316254ed4515a0ae49927eb8805577294da1e75fb87e21c3744b2ec7b4ddfe');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `attendance`
+--
+
+CREATE TABLE IF NOT EXISTS `attendance` (
+  `student_id` int(11) NOT NULL,
+  `subject_code` varchar(10) NOT NULL,
+  `from_date` date NOT NULL,
+  `to_date` date NOT NULL,
+  `attendance` int(11) DEFAULT NULL,
+  `total_classes` int(11) NOT NULL,
+  `final_submit` tinyint(4) NOT NULL DEFAULT '0',
+  `submit` tinyint(4) NOT NULL DEFAULT '0',
+  `saved` tinyint(4) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`student_id`,`subject_code`,`from_date`,`to_date`),
+  KEY `subject_code` (`subject_code`),
+  KEY `from_date` (`from_date`,`to_date`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `attendance_table`
+-- Dumping data for table `attendance`
 --
 
-INSERT INTO `attendance_table` (`program`, `semester`, `subject`, `teacher`, `std_roll_no`, `student_name`, `attendance`, `attd_date`, `TeacherId`) VALUES
-('MCS', 'Term-III', 'OOP', 'Miss Rabeal Zahra', 1, 'salma', 'P', '2015-11-11', NULL),
-('MCS', 'Term-III', 'MP', 'Sir javed', 3, 'farah', 'A', '0000-00-00', NULL),
-('MS', 'Term-IV', 'MP', 'Miss Sadia Khattak', 4, 'wasim', 'A', '0000-00-00', NULL),
-('MCS', 'Term-III', 'OOP', 'Miss Rabeal Zahra', 6, 'fared', 'A', '0000-00-00', NULL),
-('MS', 'Term-III', 'C++', 'Dr.Zubair', 9, 'salman', 'A', '0000-00-00', NULL),
-('MS', 'Term-III', 'OOP', 'Others', 21, 'farhin', 'P', '0000-00-00', NULL),
-('PHD', 'Term-I', 'Others', 'Dr.Zubair', 33, 'fff', 'A', '0000-00-00', NULL),
-('BSSC', 'Term-I', 'C++', 'Dr.Zubair', 76, 'hhhh', 'A|', '0000-00-00', NULL),
-('P.HD', 'Term-I', 'Others', 'Others', 79, 'sdsfs', 'P', '0000-00-00', NULL),
-('MS', 'Term-V', 'OOP', 'Dr.Zubair', 16, 'gggg', 'A', '0000-00-00', NULL),
-('MCS', 'Term-VIII', 'C++', 'Others', 55, 'uu', 'Array', '0000-00-00', NULL),
-('PHD', 'Term-V', 'Others', 'Others', 66, 'qqq', 'Array', '0000-00-00', NULL),
-('MS', 'Term-IV', 'MP', 'Miss Rabeal Zahra', 98, 'jj', 'A', '0000-00-00', NULL),
-('MCS', 'Term-IV', 'C++', 'Dr.Zubair', 54, 'fff', 'A', '0000-00-00', NULL),
-('BSSC', 'Term-IV', 'C++', 'Miss Sadia Khattak', 98, 'vv', 'P', '0000-00-00', NULL),
-('P.HD', 'Term-II', 'Java', 'Miss Rabeal Zahra', 87, 'sahar', 'A', '0000-00-00', NULL),
-('MS', 'Term-III', 'OOP', 'Miss Sadia Khattak', 87, 'shabir', 'A', '0000-00-00', NULL),
-('BSSC', 'Term-I', 'C++', 'Dr.Zubair', 67, 'hhhh', 'P', '0000-00-00', NULL),
-('BSCS', 'Term-III', 'Java', 'Sir javed', 41, 'Ahmad', 'A', '0000-00-00', NULL),
-('BSSC', 'Term-III', 'C++', 'Sir javed', 88, 'sasa', 'A', '0000-00-00', NULL),
-('BSCS', 'Term-I', 'C++', 'Dr.Zubair', 66, 'yuy', 'P', '0000-00-00', NULL),
-('BSSC', 'Term-II', 'Java', 'Sir javed', 17, 'farahh', 'A', '0000-00-00', NULL),
-('MCS', 'Term-III', 'MP', 'Miss Sadia Khattak', 30, 'wasim', 'A', '0000-00-00', NULL),
-('MCS', 'Term-II', 'Java', 'Sir javed', 9, 'razaq', 'P', '0000-00-00', NULL),
-('BSCS', 'Term-I', 'Java', 'Sir javed', 10, 'sardar', 'P', '0000-00-00', NULL),
-('P.HD', 'Term-II', 'OOP', 'Miss Rabeal Zahra', 16, 'noor', 'A', '0000-00-00', NULL),
-('MCS', 'Term-II', 'Java', 'Sir javed', 21, 'farhin', 'P', '0000-00-00', NULL),
-('BSCS', 'Term-II', 'MP', 'Miss Rabeal Zahra', 1, 'j', 'A', '2015-11-11', NULL),
-('MS', 'Term-V', 'OOP', 'Dr.Zubair', 123, 'dv', 'P', '0000-00-00', NULL),
-('MCS', 'Term-III', 'Others', 'Others', 16, 't', 'P', '0000-00-00', NULL),
-('BSCS', 'Term-VIII', 'OOP', 'Miss Rabeal Zahra', 1, 'rabia', 'P', '2015-11-11', NULL),
-('BSSC', 'Term-I', 'C++', 'Dr.Zubair', 18, 'usra', 'A', '0000-00-00', NULL),
-('BSCS', 'Term-V', 'MP', 'Sir javed', 1, 'hh', 'A', '2015-11-11', NULL),
-('BSSC', 'Term-I', 'C++', 'Dr.Zubair', 1, 'rezwan', 'P', '2015-11-11', NULL),
-('MCS', 'Term-I', 'OOP', 'Miss Rabeal Zahra', 2, 'Afsana', 'P', '2015-11-11', NULL),
-('MCS', 'Term-III', 'Java', 'Dr.Zubair', 2, 'Ali', 'P', '2015-11-10', NULL),
-('P.HD', 'Term-IV', 'OOP', 'Sir javed', 5, 'sajjad', 'A', '2015-11-10', NULL);
+INSERT INTO `attendance` (`student_id`, `subject_code`, `from_date`, `to_date`, `attendance`, `total_classes`, `final_submit`, `submit`, `saved`) VALUES
+(123, 'CEN-401', '2015-01-23', '2015-01-24', 1, 2, 1, 1, 0),
+(123, 'CEN-402', '2015-01-23', '2015-01-24', 1, 2, 0, 1, 0),
+(123, 'CEN-403', '2015-01-23', '2015-01-24', 2, 2, 0, 1, 0),
+(123, 'CEN-404', '2015-01-23', '2015-01-24', 2, 2, 0, 1, 0),
+(234, 'CEN-401', '2015-01-23', '2015-01-24', 2, 2, 1, 1, 0),
+(234, 'CEN-402', '2015-01-23', '2015-01-24', 1, 2, 0, 1, 0),
+(234, 'CEN-403', '2015-01-23', '2015-01-24', 1, 2, 0, 1, 0),
+(234, 'CEN-404', '2015-01-23', '2015-01-24', 1, 2, 0, 1, 0),
+(9897, 'CEN-401', '2015-01-23', '2015-01-24', 1, 2, 1, 1, 0),
+(9897, 'CEN-402', '2015-01-23', '2015-01-24', 1, 2, 0, 1, 0),
+(9897, 'CEN-403', '2015-01-23', '2015-01-24', 2, 2, 0, 1, 0),
+(9897, 'CEN-404', '2015-01-23', '2015-01-24', 2, 2, 0, 1, 0),
+(9898, 'CEN-401', '2015-01-23', '2015-01-24', 1, 2, 1, 1, 0),
+(9898, 'CEN-402', '2015-01-23', '2015-01-24', 2, 2, 0, 1, 0),
+(9898, 'CEN-403', '2015-01-23', '2015-01-24', 1, 2, 0, 1, 0),
+(9898, 'CEN-404', '2015-01-23', '2015-01-24', 2, 2, 0, 1, 0),
+(9899, 'CEN-401', '2015-01-23', '2015-01-24', 2, 2, 1, 1, 0),
+(9899, 'CEN-402', '2015-01-23', '2015-01-24', 2, 2, 0, 1, 0),
+(9899, 'CEN-403', '2015-01-23', '2015-01-24', 1, 2, 0, 1, 0),
+(9899, 'CEN-404', '2015-01-23', '2015-01-24', 1, 2, 0, 1, 0);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `semester_table`
+-- Table structure for table `ci_sessions`
 --
 
-CREATE TABLE IF NOT EXISTS `semester_table` (
-  `semester_no` varchar(32) NOT NULL,
-  `subject` varchar(64) NOT NULL
+CREATE TABLE IF NOT EXISTS `ci_sessions` (
+  `session_id` varchar(40) NOT NULL DEFAULT '''0''',
+  `ip_address` varchar(45) NOT NULL DEFAULT '''0''',
+  `user_agent` varchar(120) NOT NULL,
+  `last_activity` int(10) NOT NULL DEFAULT '0',
+  `user_data` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `semester_table`
+-- Dumping data for table `ci_sessions`
 --
 
-INSERT INTO `semester_table` (`semester_no`, `subject`) VALUES
-('Term-II', 'java');
+INSERT INTO `ci_sessions` (`session_id`, `ip_address`, `user_agent`, `last_activity`, `user_data`) VALUES
+('cbedd9686160becf40f3f9705f1215a4', '::1', 'Mozilla/5.0 (Windows NT 6.3; WOW64; rv:35.0) Gecko/20100101 Firefox/35.0', 1422083834, '');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `student_table`
+-- Table structure for table `period`
 --
 
-CREATE TABLE IF NOT EXISTS `student_table` (
-  `std_roll_no` int(11) NOT NULL AUTO_INCREMENT,
-  `student_name` varchar(32) CHARACTER SET latin1 NOT NULL,
-  `dob` date NOT NULL,
-  `gender` varchar(6) CHARACTER SET latin1 NOT NULL,
-  `email` varchar(64) CHARACTER SET latin1 NOT NULL,
-  `phone` varchar(32) CHARACTER SET latin1 DEFAULT NULL,
-  `address` varchar(255) CHARACTER SET latin1 NOT NULL,
-  `Session` varchar(40) COLLATE latin1_general_ci DEFAULT NULL,
-  `Program` varchar(40) COLLATE latin1_general_ci DEFAULT NULL,
-  `Semester` varchar(40) COLLATE latin1_general_ci DEFAULT NULL,
-  PRIMARY KEY (`std_roll_no`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci AUTO_INCREMENT=5 ;
+CREATE TABLE IF NOT EXISTS `period` (
+  `from_date` date NOT NULL,
+  `to_date` date NOT NULL,
+  `timstamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`from_date`,`to_date`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `student_table`
+-- Dumping data for table `period`
 --
 
-INSERT INTO `student_table` (`std_roll_no`, `student_name`, `dob`, `gender`, `email`, `phone`, `address`, `Session`, `Program`, `Semester`) VALUES
-(2, 'zaman', '1990-12-23', 'male', 'zohra@yahoo.com', '0345831865', 'kbkff', '2012-2015', 'Bcs', '2nd'),
-(4, 'Ali', '1992-08-06', 'male', 'ali@gmail.com', '0345831067', 'kpk-pak', '2012-2015', 'Bcs', '2nd');
+INSERT INTO `period` (`from_date`, `to_date`, `timstamp`) VALUES
+('2015-01-23', '2015-01-24', '2015-01-24 07:10:31');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `subject_table`
+-- Table structure for table `sessionals`
 --
 
-CREATE TABLE IF NOT EXISTS `subject_table` (
-  `subject_no` int(11) NOT NULL AUTO_INCREMENT,
-  `subject_name` varchar(32) NOT NULL,
-  `teacher_name` varchar(64) NOT NULL,
-  `field` varchar(8) NOT NULL,
-  `semester` varchar(32) NOT NULL,
-  PRIMARY KEY (`subject_no`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=9 ;
-
---
--- Dumping data for table `subject_table`
---
-
-INSERT INTO `subject_table` (`subject_no`, `subject_name`, `teacher_name`, `field`, `semester`) VALUES
-(1, 'C++', 'Sir javed', 'BSCS', '4th'),
-(2, 'OOP', 'Dr.Zubair', 'MCS', '3rd'),
-(3, 'MP', 'Dr.Zubair', 'BSCS', '6th'),
-(4, 'OOP', 'Miss Sadia Khattak', 'BSCS', '5th'),
-(5, 'C++', 'Miss Rabeal Zahra', 'BSCS', '2nd'),
-(6, 'C++', 'Others', 'BSCS', '2nd'),
-(7, 'Java', 'Miss Sadia Khattak', 'MCS', '3rd'),
-(8, 'Network', 'TEst', 'CS', '7');
+CREATE TABLE IF NOT EXISTS `sessionals` (
+  `subject_code` varchar(10) NOT NULL,
+  `student_id` int(11) NOT NULL,
+  `current_year` year(4) NOT NULL,
+  `type` tinyint(4) NOT NULL,
+  `total_marks` int(11) NOT NULL,
+  `marks` int(11) NOT NULL,
+  `timstamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`subject_code`,`student_id`,`current_year`,`type`),
+  KEY `student_id` (`student_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `tbl_attendence`
+-- Table structure for table `student`
 --
 
-CREATE TABLE IF NOT EXISTS `tbl_attendence` (
-  `attID` int(11) NOT NULL AUTO_INCREMENT,
-  `StudentRollNumber` int(11) NOT NULL,
-  `SubjectId` int(11) NOT NULL,
-  `Attendence` varchar(11) NOT NULL,
-  `Date` datetime NOT NULL,
-  PRIMARY KEY (`attID`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=19 ;
+CREATE TABLE IF NOT EXISTS `student` (
+  `student_id` int(11) NOT NULL,
+  `roll_number` varchar(10) NOT NULL,
+  `student_name` varchar(40) NOT NULL,
+  `semester` tinyint(1) NOT NULL,
+  `batch` year(4) NOT NULL,
+  PRIMARY KEY (`student_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `tbl_attendence`
+-- Dumping data for table `student`
 --
 
-INSERT INTO `tbl_attendence` (`attID`, `StudentRollNumber`, `SubjectId`, `Attendence`, `Date`) VALUES
-(1, 2, 1, 'P', '2015-12-01 00:00:00'),
-(2, 2, 1, 'P', '2015-12-02 00:00:00'),
-(3, 2, 1, 'A', '2015-12-01 00:00:00'),
-(4, 2, 1, 'A', '2015-12-03 00:00:00'),
-(5, 2, 1, 'P', '2015-12-04 00:00:00'),
-(6, 4, 2, 'A', '2015-12-10 00:00:00'),
-(7, 4, 3, 'A', '2015-12-10 00:00:00'),
-(8, 4, 2, 'A', '2015-12-17 00:00:00'),
-(9, 4, 3, 'A', '2015-12-15 00:00:00'),
-(10, 4, 3, 'A', '2015-12-15 00:00:00'),
-(11, 4, 7, 'A', '2015-12-01 00:00:00'),
-(12, 2, 8, 'P', '2015-12-11 00:00:00'),
-(13, 2, 8, 'A', '2015-12-18 00:00:00'),
-(14, 5, 7, 'P', '2015-12-24 08:58:20'),
-(15, 5, 8, 'A', '2015-12-24 08:58:45'),
-(16, 4, 4, 'A', '2015-12-24 09:14:09'),
-(17, 4, 4, 'A', '2015-12-24 09:14:31'),
-(18, 2, 8, 'P', '2015-12-25 11:44:15');
+INSERT INTO `student` (`student_id`, `roll_number`, `student_name`, `semester`, `batch`) VALUES
+(123, '12CSS13', 'John Guttag', 4, 1997),
+(234, '15CSS78', 'Bob Marley', 4, 1998),
+(9897, '10sww77', 'Peter', 4, 2012),
+(9898, '10mpp78', 'Harry', 4, 2012),
+(9899, '10sww78', 'Tom', 4, 2012);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `teacher_table`
+-- Table structure for table `studies`
 --
 
-CREATE TABLE IF NOT EXISTS `teacher_table` (
+CREATE TABLE IF NOT EXISTS `studies` (
+  `student_id` int(11) NOT NULL,
+  `subject_code` varchar(10) NOT NULL,
+  PRIMARY KEY (`student_id`,`subject_code`),
+  KEY `subject_code` (`subject_code`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `subject`
+--
+
+CREATE TABLE IF NOT EXISTS `subject` (
+  `subject_code` varchar(10) NOT NULL,
+  `subject_name` varchar(50) NOT NULL,
+  `subject_abbr` varchar(10) NOT NULL,
+  `semester` tinyint(1) NOT NULL,
+  `teacher_id` int(11) NOT NULL,
+  PRIMARY KEY (`subject_code`),
+  KEY `teacher_id` (`teacher_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `subject`
+--
+
+INSERT INTO `subject` (`subject_code`, `subject_name`, `subject_abbr`, `semester`, `teacher_id`) VALUES
+('CEN-301', 'Data Structures and Programming', 'DS', 3, 0),
+('CEN-302', 'Digital Logic Theory', 'DLT', 3, 0),
+('CEN-303', 'Discrete Mathematics', 'DM', 3, 0),
+('CEN-304', 'Electronic Devices and Applications', 'EDA', 3, 0),
+('CEN-305', 'Mathematics-1', 'MA', 3, 0),
+('CEN-306', 'Signals and Systems', 'SS', 3, 0),
+('CEN-401', 'Computer Organization', 'CO', 4, 41),
+('CEN-402', 'Operating System - 1', 'OS1', 4, 41),
+('CEN-403', 'Information Technology', 'IT', 4, 41),
+('CEN-404', 'Analog and Digital Communication', 'ADC', 4, 41),
+('CEN-405', 'Mathematics-2', 'MA2', 4, 0),
+('CEN-406', 'System Software', 'SS', 4, 0),
+('CEN-501', 'Computer Architecture', 'CA', 5, 0),
+('CEN-502', 'Automata Theory', 'AT', 5, 0),
+('CEN-503', 'Computer Network-1', 'CN1', 5, 0),
+('CEN-504', 'Database Systems', 'DBMS', 5, 0),
+('CEN-505', 'Microprocessor', 'MC', 5, 0),
+('CEN-506', 'Operating Systems-2', 'OS2', 5, 0),
+('CEN-701', 'Internet Fundamentals', 'IF', 7, 0),
+('CEN-702', 'Management Science', 'MS', 7, 0),
+('CEN-703', 'Language Proccessor 2', 'LP2', 7, 0),
+('CEN-704', 'Mobile Communication', 'MC', 7, 0),
+('CEN-705', 'Data Mining', 'DM', 7, 0),
+('CEN-706', 'Embedded System', 'ES', 7, 0),
+('CEN-801', 'Advanced algorithms & current trends in computing', 'AACTC', 8, 0),
+('CEN-802', 'Artificial Intelligence', 'AI', 8, 0),
+('CEN-803', 'Software Project Management', 'SPM', 8, 0),
+('CEN-804', 'Distributed Processing', 'DP', 8, 0),
+('CS-201', 'Fundamentals of Computing', 'FC', 2, 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `teacher`
+--
+
+CREATE TABLE IF NOT EXISTS `teacher` (
   `teacher_id` int(11) NOT NULL AUTO_INCREMENT,
-  `first_name` varchar(64) NOT NULL,
-  `last_name` varchar(64) NOT NULL,
-  `dob` date NOT NULL,
-  `gender` varchar(8) NOT NULL,
-  `email` varchar(64) NOT NULL,
-  `phone` varchar(32) DEFAULT NULL,
-  `degree` varchar(32) NOT NULL,
-  `salary` varchar(64) NOT NULL,
-  `address` text NOT NULL,
+  `teacher_name` varchar(40) NOT NULL,
+  `email` varchar(40) NOT NULL,
+  `username` varchar(40) NOT NULL,
+  `password` varchar(130) NOT NULL,
   PRIMARY KEY (`teacher_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8mb4 AUTO_INCREMENT=6 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=42 ;
 
 --
--- Dumping data for table `teacher_table`
+-- Dumping data for table `teacher`
 --
 
-INSERT INTO `teacher_table` (`teacher_id`, `first_name`, `last_name`, `dob`, `gender`, `email`, `phone`, `degree`, `salary`, `address`) VALUES
-(2, 'Ahmad', 'Javed', '1983-02-12', '-------s', 'javed@yahoo.com', '03312567814', '-------select-------', '70000', 'kohat-pakistan'),
-(3, 'Sadia', 'Khattak', '1983-02-12', 'Female', 'khattak@yahoo.com', '033125678786', 'Bachelor', '1200000', 'kpk-peshawar'),
-(4, 'Rabeal', ' Zahra', '2014-07-22', 'Female', 'rab@gmail.com', '------', 'MS', '40000', 'di khan'),
-(5, 'Zubair', 'ahmad', '2015-12-18', 'male', 'zubair@gmai.com', '9089998', 'MS', '80000000', 'Di khan');
+INSERT INTO `teacher` (`teacher_id`, `teacher_name`, `email`, `username`, `password`) VALUES
+(0, 'void', 'zishanrbp@gmail.com', 'void', 'void'),
+(41, 'Teacher 1', '', 'teacher', '01fb5a6bfeeac3e520094442c9eddc34471c2a907a56b9a6baddcaf390def046e1daf65dd41b238fd03cdd4c95012802f44cb059497dbd5a16c00aa79e355aa6');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `total_attendance`
+--
+
+CREATE TABLE IF NOT EXISTS `total_attendance` (
+  `student_id` int(11) NOT NULL,
+  `subject_code` varchar(10) NOT NULL,
+  `attendance` int(11) NOT NULL,
+  `semester` tinyint(1) NOT NULL,
+  `batch` year(4) NOT NULL,
+  `total_classes` int(11) NOT NULL,
+  `year` year(4) NOT NULL,
+  PRIMARY KEY (`student_id`,`subject_code`,`semester`,`batch`,`year`),
+  KEY `subject_code` (`subject_code`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `attendance`
+--
+ALTER TABLE `attendance`
+  ADD CONSTRAINT `attendance_ibfk_1` FOREIGN KEY (`student_id`) REFERENCES `student` (`student_id`),
+  ADD CONSTRAINT `attendance_ibfk_2` FOREIGN KEY (`subject_code`) REFERENCES `subject` (`subject_code`),
+  ADD CONSTRAINT `attendance_ibfk_3` FOREIGN KEY (`from_date`) REFERENCES `period` (`from_date`),
+  ADD CONSTRAINT `attendance_ibfk_4` FOREIGN KEY (`from_date`, `to_date`) REFERENCES `period` (`from_date`, `to_date`);
+
+--
+-- Constraints for table `sessionals`
+--
+ALTER TABLE `sessionals`
+  ADD CONSTRAINT `sessionals_ibfk_1` FOREIGN KEY (`student_id`) REFERENCES `student` (`student_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `sessionals_ibfk_2` FOREIGN KEY (`subject_code`) REFERENCES `subject` (`subject_code`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `studies`
+--
+ALTER TABLE `studies`
+  ADD CONSTRAINT `studies_ibfk_1` FOREIGN KEY (`student_id`) REFERENCES `student` (`student_id`),
+  ADD CONSTRAINT `studies_ibfk_2` FOREIGN KEY (`subject_code`) REFERENCES `subject` (`subject_code`);
+
+--
+-- Constraints for table `subject`
+--
+ALTER TABLE `subject`
+  ADD CONSTRAINT `subject_ibfk_1` FOREIGN KEY (`teacher_id`) REFERENCES `teacher` (`teacher_id`);
+
+--
+-- Constraints for table `total_attendance`
+--
+ALTER TABLE `total_attendance`
+  ADD CONSTRAINT `total_attendance_ibfk_1` FOREIGN KEY (`student_id`) REFERENCES `student` (`student_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `total_attendance_ibfk_2` FOREIGN KEY (`subject_code`) REFERENCES `subject` (`subject_code`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
