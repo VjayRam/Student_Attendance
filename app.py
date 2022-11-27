6bacef9ea9982f6aefd7bc1bada991ed16f822a3
VjayRam <vjayram.enag@gmail.com>
2022-11-27 21:46:18 +0530

    -

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import mysql.connector
 from database import about
-from database import create_table, edit_details
+from database import create_table, add_details
 
 def main():
     st.title("Student Attendance System")
@@ -11,7 +11,7 @@ def main():
     if ch == "Attendance":
         about()
     elif ch == "Add Details":
-        about()
+        add_details()
     elif ch == "View Details":
         about()
     elif ch == "Update Details":
--- database.py
+++ database.py
@@ -1,12 +1,8 @@
 import mysql.connector
 import streamlit as st
+from datetime import date
 
-mydb = mysql.connector.connect(
-    host="localhost",
-    user="root",
-    password="root",
-    database="attendance_se"
-)
+mydb = mysql.connector.connect(host="localhost", user="root", password="root", database="attendance_se")
 c = mydb.cursor()
 
 def create_table():
@@ -16,12 +12,50 @@ def create_table():
     c.execute("CREATE TABLE IF NOT EXISTS Student_Subject ( Student_ID varchar(10), Student_name varchar(20), Sub_ID varchar(10), Class int, foreign key(Student_ID) references Student(Student_ID), foreign key(Sub_ID) references Subjects(Sub_ID));")
     c.execute("CREATE TABLE IF NOT EXISTS Subject_Teacher (Sub_ID varchar(10), Teacher_ID varchar(10), Class int, foreign key(Sub_ID) references Subjects(Sub_ID), foreign key(Teacher_ID) references Teacher(Teacher_ID));")
     c.execute("CREATE TABLE IF NOT EXISTS Attendance (Student_ID varchar(10), Class int, Section varchar(5), Sub_ID varchar(10), Teacher_ID varchar(10), Class_DateTime datetime, Attendance char default 'N', foreign key(Teacher_ID) references Teacher(Teacher_ID), foreign key(Sub_ID) references Subjects(Sub_ID), foreign key(Student_ID) references Student(Student_ID));")
-  
+
 def add_details():
-    opt = ["Add Student", "Add Subject", "Add Teacher"]
-    x = st.selectbox(menu = opt)
+    opt = ["Add Student", "Add Teacher", "Add Subject"]
+    x = st.selectbox("Add:",opt)
     if x == "Add Student":
-        c.execute("")
+        st.subheader("Enter Student Details:")
+        
+        col1, col2 = st.columns(2)
+        with col1:
+            sid = st.text_input("Enter Student ID: ")
+            fname = st.text_input("Enter First Name: ")
+            lname = st.text_input("Enter Last Name: ")
+            cl = st.radio("Choose Class: ",["1","2","3","4","5","6","7","8","9","10","11","12"])
+        with col2:
+            dob = st.date_input("Enter DOB: ")
+            pin = st.text_input("Enter Pincode: ")
+            ph = st.number_input("Enter Phone Number: ")
+            sec = st.radio("Choose Section: ",["A","B","C","D","E","F","G","H","I","K","L"]) 
+        today = date.today()
+        age = today.year - dob.year - ((today.month, today.day) < (dob.month, dob.day)) 
+        
+        if st.button("Add Student"):
+            c.execute("insert into student( Student_ID ,Fname,Lname ,Age ,DOB ,Pincode ,Class ,Section ,Parent_ph) values(%s,%s,%s,%s,%s,%s,%s,%s,%s);", (sid, fname, lname, age, dob, pin, cl, sec, ph))
+            st.success("Successfully added student: {}".format(sid))
+            
+    elif x == "Add Teacher":
+        st.subheader("Enter Teacher Details:")
+        col1, col2 = st.columns(2)
+        with col1:
+            tid = st.text_input("Enter Teacher ID: ")
+            fname = st.text_input("Enter First Name: ")
+            lname = st.text_input("Enter Last Name: ")
+            age = st.number_input("Enter Age: ")
+            email = st.text_input("Enter Email: ")
+        with col2:
+            cl = st.radio("Choose Class: ",["1","2","3","4","5","6","7","8","9","10","11","12"])
+            ph = st.number_input("Enter Phone Number: ")
+        if st.button("Add Teacher"):
+            c.execute("insert into Teacher( Teacher_ID ,Fname,Lname ,Age ,Class ,Phone ,Email) values(%s,%s,%s,%s,%s,%s,%s,%s,%s);", (tid, fname, lname, age, cl, ph, email))
+            st.success("Successfully added teacher: {}".format(tid))
+    else:
+        pass
+
+
 
 
 def about():
